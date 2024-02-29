71d7c4f09e513c8bb278d01eb4815cbd52e05861
afifrohul <afifmemyself22@gmail.com>
2024-02-29 11:20:48 +0700

    edit .env.example

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,5 +1,7 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
+@if (backpack_user()->hasRole('superadmin'))
 <x-backpack::menu-item title="Users" icon="la la-user" :link="backpack_url('user')" />
+@endif
 <x-backpack::menu-item title="Shortlinks" icon="la la-link" :link="backpack_url('shortlink')" />
